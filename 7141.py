b5e302f6b318ce7cac703b2cbcbad09b232bcca8
QwQ <bamon60155@gmail.com>
2026-07-17 09:45:59 +0800

    自己尝试

--- 7141.py
+++ 7141.py
@@ -3,8 +3,8 @@ print ("我很想你")
 print ("施施我很想你呀")
 # 打开txt文件
 # 模式有三种，r，w（内容如果存在会被清空重写），a（追加append）
-f = open("文件名.txt", "模式", encoding="utf-8")
-f.close()
+#f = open("文件名.txt", "模式", encoding="utf-8")
+#f.close()
 # 更推荐的另一种写法 自动帮我关闭文件
 # 写txt文件
 with open("test.txt", "w", encoding="utf-8") as f:
@@ -15,6 +15,14 @@ with open("test.txt", "r", encoding="utf-8") as f:
     # read()函数是吧整个文件内容读成一个字符串
     # readlines()是把每一行读成列表的一个元素
     print(content)
-    
+
+#自己尝试
+with open("note.txt", "w", encoding="utf-8") as f:
+    f.write("我想我会一直孤单")
+
+with open("note.txt", "r", encoding="utf-8") as f:
+    note = f.read()
+    print(note)
+
 
 
--- add_new.py
+++ add_new.py
@@ -9,3 +9,12 @@ with open("data.json", "w", encoding="utf-8") as f:
 with open("data.json", "r", encoding="utf-8") as f:
     loaded_data = json.load(f)
     print(loaded_data)
+
+#自己尝试
+info = {"name": "糯米", "age": 3, "sex": "female", "skill": "可爱"}
+with open("info.json", "w", encoding="utf-8") as f:
+    json.dump(info, f, ensure_ascii=False, indent=2)
+
+with open("info.json", "r", encoding="utf-8") as f:
+    message = json.load(f)
+    print(message)
